49151918b39c8d6466ccfaa027dfb5a0d86d70c6
sirekanyan <sirekanyan@gmail.com>
2014-06-02 07:52:08 +0400

    commit 6

--- vk-offline-gtk.rb
+++ vk-offline-gtk.rb
@@ -37,7 +37,7 @@ def parse_uid username
 end
 
 def make_box text_label, entity
-  entity.width_chars = 30
+  entity.width_chars = 40
   label = Gtk::Label.new(text_label)
   label.width_chars = $labels_size
   label.set_alignment(0, 0.5)
@@ -84,51 +84,39 @@ end
 def make_history_button text, user, history
   button = Gtk::Button.new(text)
   button.signal_connect("clicked") {
-    if $history_opened then
-      history.hide
-      button.label = 'Show history'
-      $history_opened = false
-    else
-      history.show
-      history.buffer.text = "getting..."
-      Thread.new {
-        uid = parse_uid(user.text)
-        buff = ""
-        usrname = ""
-        if uid then
-          buff = $vk.messages_getHistory(:user_id => uid)
-          usrname = $vk.user(uid)['first_name']
+    history.buffer.text = "getting..."
+    Thread.new {
+      uid = parse_uid(user.text)
+      buff = ""
+      usrname = ""
+      if uid then
+        buff = $vk.messages_getHistory(:user_id => uid)
+        usrname = $vk.user(uid)['first_name']
+      end
+      buff_temp = "Total: " + buff.shift.to_s + "\n\n"
+      buff.each do |msg|
+        if msg['out'] == 1 then
+          who = "Me:\n"
+        else
+          who = "#{usrname}:\n"
         end
-        buff_temp = "Total: " + buff.shift.to_s + "\n\n"
-        buff.each do |msg|
-          if msg['out'] == 1 then
-            who = "Me:\n"
-          else
-            who = "#{usrname}:\n"
-          end
-          attach = ""
-          if msg['attachment'] then
-            attach = "(attachment)\n"
-          end
-          body = msg['body']
-          body += "\n" unless body.empty?
-          buff_temp += who + body + attach + "\n"
+        attach = ""
+        if msg['attachment'] then
+          attach = "(attachment)\n"
         end
-        history.buffer.text = buff_temp
-        history.buffer.text = "(no messages yet)" if buff.to_s == "[0]"
-      }
-      button.label = 'Hide history'
-      $history_opened = true
-    end
-
+        body = msg['body']
+        body += "\n" unless body.empty?
+        buff_temp += who + body + attach + "\n"
+      end
+      history.buffer.text = buff_temp
+      history.buffer.text = "(no messages yet)" if buff.to_s == "[0]"
+    }
   }
   button
 end
 
-$history_opened = false
-
 window = Gtk::Window.new
-window.set_title  "Send message"
+window.set_title  "Offline Messenger"
 window.border_width = 10
 window.signal_connect('delete_event') { Gtk.main_quit }
 
@@ -145,12 +133,7 @@ history = Gtk::TextView.new
 history.left_margin = 10
 history.editable = false
 history.wrap_mode = Gtk::TextTag::WRAP_WORD
-history.hide
-i = 0
-history.signal_connect("show") {
-  history.hide if i == 0
-  i = 1
-}
+
 buttonsbox = Gtk::HBox.new(false, 0)
 buttonsbox.pack_start(make_send_button("Send message", user, msg), false, false, 3)
 buttonsbox.pack_start(make_history_button("Show history", user, history), false, false, 3)
