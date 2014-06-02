fd55038c576eca62f03c42f5f8d1318034e67ce8
sirekanyan <sirekanyan@gmail.com>
2014-06-02 07:51:26 +0400

    commit 2

--- vk-offline-gtk.rb
+++ vk-offline-gtk.rb
@@ -1,27 +1,37 @@
 #!/usr/bin/env ruby
 require 'gtk2'
 
-button = Gtk::Button.new("Hello World")
-
-button.signal_connect("clicked") {
-  puts "Hello World"
-}
+def callback(widget)
+  puts "Hello again - #{widget.label}(#{widget}) was pressed."
+end
 
 window = Gtk::Window.new
 
-window.signal_connect("delete_event") {
-  puts "delete event occurred"
-  #true
-  false
-}
-
-window.signal_connect("destroy") {
-  puts "destroy event occurred"
-  Gtk.main_quit
-}
-
+window.title = "Hello Buttons"
 window.border_width = 10
-window.add(button)
-window.show_all
 
+window.signal_connect('delete_event') do
+  Gtk.main_quit
+  false
+end
+
+box1 = Gtk::HBox.new(false, 0)
+
+window.add(box1)
+
+button1 = Gtk::Button.new("Button 1")
+button2 = Gtk::Button.new("Button 2")
+
+button1.signal_connect( "clicked" ) do |w|
+  callback(w)
+end
+
+button2.signal_connect("clicked") do |w|
+  callback(w)
+end
+
+box1.pack_start(button1, true, true, 0)
+box1.pack_start(button2, true, true, 0)
+
+window.show_all
 Gtk.main
